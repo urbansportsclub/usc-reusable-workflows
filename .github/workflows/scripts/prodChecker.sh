ce127106201af8189ee1dbc71dab306fd7e18dbd
Gerson Trujillo <gerson.trujillo@urbansportsclub.com>
2024-09-23 12:27:15 +0200

    comment

--- .github/workflows/scripts/prodChecker.sh
+++ .github/workflows/scripts/prodChecker.sh
@@ -4,6 +4,6 @@ if [ -f "catalog-info.yaml" ]; then
   echo "Catalog file found."
   exit 0
 else
-  echo "The catalog file is missing, more information here: https://backstage.dev.urbansportsclub.tech/docs/default/component/devx-playground"
+  echo "Catalog file missing, more information here: https://backstage.dev.urbansportsclub.tech/docs/default/component/devx-playground"
   exit 1
 fi
