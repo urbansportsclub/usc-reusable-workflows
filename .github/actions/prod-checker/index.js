26530d8a250ac1d6ef401157d6b448a8d0def674
Gerson Trujillo <gerson.trujillo@urbansportsclub.com>
2024-09-23 13:43:09 +0200

    using custom github action

---
+++ .github/actions/prod-checker/index.js
@@ -0,0 +1,11 @@
+import { existsSync } from 'fs';
+
+const filePath = 'catalog-info.yaml';
+
+if (existsSync(filePath)) {
+  console.log('Catalog file found.');
+  process.exit(0);
+} else {
+  console.log('Catalog file missing, more information here: https://backstage.dev.urbansportsclub.tech/docs/default/component/devx-playground');
+  process.exit(1);
+}
--- .github/workflows/scripts/prodChecker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -f "catalog-info.yaml" ]; then
-  echo "Catalog file found."
-  exit 0
-else
-  echo "Catalog file missing, more information here: https://backstage.dev.urbansportsclub.tech/docs/default/component/devx-playground"
-  exit 1
-fi
