a0c87639961fd7d368e674c03ee4cd89379d446a
Gerson Trujillo <gerson.trujillo@urbansportsclub.com>
2024-10-03 12:25:15 +0200

    SCE validator action, first scratch

---
+++ .github/actions/SCE-validator/index.js
@@ -0,0 +1,56 @@
+const yaml = require('js-yaml');
+const fs   = require('fs');
+
+const filePath = 'catalog-info.yaml';
+const tagsList = ['go', 'javascript'];
+const systemsList = ['payments', 'internal-libraries', 'internal-services']; // The systems of USC services
+let doc;
+// if (!existsSync(filePath)) {
+//   console.log('Catalog file missing, more information here: https://backstage.dev.urbansportsclub.tech/docs/default/component/devx-playground/catalog/');
+//   process.exit(1);
+// }
+
+// Get catalog file, or throw exception if not found
+try {
+  doc = yaml.load(fs.readFileSync(filePath, 'utf8')); // todo make it constant
+} catch (e) {
+  console.log('Catalog file missing, more information here: https://backstage.dev.urbansportsclub.tech/docs/default/component/devx-playground/catalog/');
+  process.exit(1);
+}
+
+console.log(doc) ;
+
+//
+// const fileContent = readFileSync(filePath, 'utf8');
+// const catalogFiles = yaml.loadAll(fileContent);
+//
+// const catalogFile = catalogFiles.find(doc => doc.spec && doc.spec.tags && doc.spec.system);
+//
+// if (!catalogFile) {
+//   console.log('No valid document found in catalog file.');
+//   process.exit(1);
+// }
+//
+// const { system, tags } = catalogFile.spec;
+//
+// if (!tags.every(tag => tagsList.includes(tag))) {
+//   console.log('Tag missing, more information here: https://backstage.dev.urbansportsclub.tech/docs/default/component/devx-playground/tags');
+//   process.exit(1);
+// }
+//
+// if (!systemsList.includes(system)) {
+//   console.log('System missing, more information here: https://backstage.dev.urbansportsclub.tech/docs/default/component/devx-playground/systems');
+//   process.exit(1);
+// }
+//
+// if (tags.includes('go')) {
+//   // Call go-validator.js
+// }
+//
+// if (tags.includes('javascript')) {
+//   // Call javascript-validator.js
+// }
+//
+// if (system === 'payments') {
+//   // Call payments-validator.js
+// }
